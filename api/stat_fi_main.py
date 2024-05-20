ee6fae1267b05ef1994f08091975dce769df3b92
ihamuma <markus.ihamuotila@gmail.com>
2024-05-20 16:55:19 +0200

    Enabled building data files from main.py. Fixed importing errors. Made stat_fi_main less verbose (some restructuring still needed)

--- api/stat_fi_main.py
+++ api/stat_fi_main.py
@@ -1,24 +1,29 @@
-from stat_fi_api_helpers import get_migration_data, get_employment_data
-import stat_fi_queries
+from .stat_fi_api_helpers import get_migration_data, get_employment_data
+from .stat_fi_queries import migration_url, migration_query, age_values, employment_url, employment_query
 import os
 
-migration_file_path = os.path.join('raw_data', 'migration.parquet')
-employment_file_path = os.path.join('raw_data', 'employment.parquet')
+def verify_and_create_files():
+    migration_file_path = os.path.join('raw_data', 'migration.parquet')
+    employment_file_path = os.path.join('raw_data', 'employment.parquet')
 
-if not os.path.isfile(migration_file_path):
-    migr_url = stat_fi_queries.migration_url
-    migr_query = stat_fi_queries.migration_query
-    migr_ages = stat_fi_queries.age_values["selection"]["values"]
+    if not os.path.isfile(migration_file_path):
+        print('No migration data file found. Creating API requests.')
 
-    migration_df = get_migration_data(migr_url, migr_query, migr_ages)
-    migration_df.to_parquet(migration_file_path, engine='pyarrow')
+        migration_ages = age_values["selection"]["values"]
 
-if not os.path.isfile(employment_file_path):
-    empl_url = stat_fi_queries.employment_url
-    empl_query = stat_fi_queries.employment_query
-    
-    employment_df = get_employment_data(empl_url, empl_query)
-    employment_df.to_parquet(employment_file_path, engine='pyarrow')
+        migration_df = get_migration_data(migration_url, migration_query, migration_ages)
+        migration_df.to_parquet(migration_file_path, engine='pyarrow')
 
+        print(f'Migration data successfully written to {migration_file_path}')
+    else:
+        print(f'Migration data file found at {migration_file_path}')
 
+    if not os.path.isfile(employment_file_path):
+        print('No migration data file found. Creating API request.')
+        
+        employment_df = get_employment_data(employment_url, employment_query)
+        employment_df.to_parquet(employment_file_path, engine='pyarrow')
 
+        print(f'Employment data successfully written to {employment_file_path}')
+    else:
+        print(f'Employment data file found at {employment_file_path}')
--- main.py
+++ main.py
@@ -1,2 +1,6 @@
-# This will form the main executing methods of the project. For now, it's a bit empty.
-print('Hello world!')
+from api.stat_fi_main import verify_and_create_files
+
+# To initialise the project, run this program to verify the existence of the migration.parquet and employment.parquet files.
+# If no files are found, the program automatically creates API requests to create the files. This may take a few moments.
+
+verify_and_create_files()
