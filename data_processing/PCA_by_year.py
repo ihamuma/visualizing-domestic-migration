3a37458eb707f99e5f200271727e37b35e90c9db
ihamuma <markus.ihamuotila@gmail.com>
2024-05-27 13:46:14 +0200

    Made minor clarifying addition to readme

--- data_processing/PCA_by_year.py
+++ data_processing/PCA_by_year.py
@@ -13,7 +13,6 @@ migration_df.loc[:, 'new_column'] = migration_df.apply(lambda row: row['Region o
 
 transformed_migration_df = migration_df.pivot_table(index='Year', columns='new_column', values='Value')
 transformed_migration_df = transformed_migration_df.reset_index()
-print(transformed_migration_df.describe())
 
 migration_file_path = os.path.join('processed_data', 'migration_transformed_pca_by_year.parquet')
 transformed_migration_df.to_parquet(migration_file_path, engine='pyarrow')
