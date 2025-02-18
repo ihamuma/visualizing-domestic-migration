93a1241e6483bc5449a2b4d183b126bcf31ef340
ihamuma <markus.ihamuotila@gmail.com>
2025-02-18 15:45:56 +0200

    Format employment mapping

--- scripts/employment_mapping_wrangling.py
+++ scripts/employment_mapping_wrangling.py
@@ -1,14 +1,14 @@
 import json
 import os
 
-with open('raw_data/employment_data_mapping_raw.json') as file:
+with open("raw_data/employment_data_mapping_raw.json") as file:
     data = json.load(file)
 
-values = data['variables'][0]['values']
-value_texts = data['variables'][0]['valueTexts']
+values = data["variables"][0]["values"]
+value_texts = data["variables"][0]["valueTexts"]
 
 value_text_mapping = dict(zip(values, value_texts))
 
-file_path = os.path.join('processed_data', 'employment_data_mapping.json')
-with open(file_path, 'w') as json_file:
+file_path = os.path.join("processed_data", "employment_data_mapping.json")
+with open(file_path, "w") as json_file:
     json.dump(value_text_mapping, json_file, indent=4)
